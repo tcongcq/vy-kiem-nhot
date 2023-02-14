19e3d7a0e853f47e89f3a9f0511f2a3eaf343dc7
Thanh Cong Nguyen <congnt@vitalify.asia>
2023-02-14 22:24:00 +0700

    fix title

--- resources/views/site/layouts/home.blade.php
+++ resources/views/site/layouts/home.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Home || Auto Care || Responsive HTML 5 Template</title>
+    <title>Home || Sinopec</title>
     <!-- responsive meta -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 

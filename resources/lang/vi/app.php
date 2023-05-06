3fd3ce16cf4fb5f3ae49a5fcdf384b786249c678
Thanh Cong Nguyen <congnt@vitalify.asia>
2023-05-07 00:41:39 +0700

    add menu trans

--- resources/lang/vi/app.php
+++ resources/lang/vi/app.php
@@ -5,6 +5,25 @@ return [
     
     'dashboard' => 'Dashboard',
 
-    'homepage'  => 'Trang chủ'
+    'homepage'  => 'Trang chủ',
+
+    'post-management'   => 'Quản lý bài viết',
+    'post'              => 'Bài viết',
+    'post-category'     => 'Danh mục bài viết',
+
+    'product-management'    => 'Quản lý sản phẩm',
+    'product'               => 'Sản phẩm',
+    'product-category'      => 'Danh mục sản phẩm',
+
+    'contact-management'    => 'Quản lý liên hệ',
+    'contact-us'            => 'Khách liên hệ',
+    'contact-info'          => 'Thông tin liên hệ',
+
+    'setting-management'    => 'Quản lý cài đặt',
+    'config-content'        => 'Cài đặt thông tin',
+    'setting'               => 'Cài đặt hệ thống',
+    'language'              => 'Ngôn ngữ',
+
+    'account-management'    => 'Quản lý tài khoản',
 ];
 
